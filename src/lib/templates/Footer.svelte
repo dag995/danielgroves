9fa6716812ecc7a8ad82921a0b166cf0af28cba9
dag995 <72547889+dag995@users.noreply.github.com>
2025-08-13 17:25:41 +0100

    Cloudflare pages

---
+++ src/lib/templates/Footer.svelte
@@ -0,0 +1,223 @@
+<script>
+  import { page } from '$app/stores'
+</script>
+
+<footer class="main-footer">
+
+  <div class="wrap">
+
+    <div class="col">
+      <a 
+        href="/" 
+        title="Daniel Groves"
+        aria-label='Home'
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 350 500" style="enable-background:new 0 0 350 500;" xml:space="preserve">
+          <path style="fill:#062E4B;" d="M337.4,50c-41.5,0-89.1-6.1-155.9-46.1L175,0l-6.5,3.9C101.7,43.9,54.1,50,12.6,50H0v302.9
+            c0,28.6,15.4,55.2,40.1,69.5L175,500l134.9-77.7c24.8-14.3,40.1-40.9,40.1-69.5V50H337.4z M325.1,352.9c0,20.1-10.8,38.8-28.2,48.8
+            L175,471.9L53.2,401.6c-17.4-10-28.2-28.7-28.2-48.8L25,75c41-1.1,87-7.2,150-43.8C238,67.8,284,73.9,325.1,75V352.9z"/>
+          <path style="fill:#062E4B;" d="M204.9,156.3c-30.5,0-62.8-0.1-93.3,0h-2.2v13c0,12,5.3,23.9,14.2,32c10.2,9.3,22.5,11.2,35.8,11.2h0h43.7
+            c3.5,0,6.2,2.8,6.2,6.2s-2.8,6.2-6.2,6.2H161h-2.3c-18.5,0.7-38.4,2.9-54.5,13.1c-28.8,18.1-36.2,58.8-22.7,88.6
+            c9.1,20,28.4,33.1,49.7,36.2c16.4,2.4,35.4,1.6,49.1-8.9c7.6-5.8,12.7-14.3,15.1-23.5c2-7.6,1.5-16.6,1.4-24.3h-40.7
+            c-6.9,0-12.5-5.6-12.5-12.5s5.6-12.5,12.5-12.5c0,0,52.4,0,53.1,0c0,0,0,0,0,0v81.2h56.2l0-145.7
+            C265.6,183.2,238.4,156.3,204.9,156.3z"/>
+          <path style="fill:#062E4B;" d="M212.5,105.9c-3.9,1.3-8,1.6-12,0.9c-20.6-3.6-25.5-25.5-25.5-25.5s-4.9,21.9-25.5,25.5c-4,0.7-8.1,0.4-12-0.9
+            c-3.6-1.2-6.3-2.8-6.3-2.8l12.5,28.1c0,0,19,0,31.3,0s31.2,0,31.2,0l12.5-28.1C218.8,103.1,216.1,104.7,212.5,105.9z"/>
+        </svg>
+      </a>
+    </div>
+
+    <div class="col">
+      <h5>Discover</h5>
+      <ul>
+        <li>Alfaholics-R parts</li>
+        <li>Workshop</li>
+        <li>About us</li>
+        <li>Stories</li>
+      </ul>
+
+      <h5 class="mt-24">Support</h5>
+      <ul>
+        <li>Contact</li>
+        <li>Shipping</li>
+        <li>Returns Policy</li>
+      </ul>
+    </div>
+    
+    <div class="col">
+      <h5>Shop</h5>
+      <ul>
+        <li><a href="/parts" title="Parts">Parts</a></li>
+        <li><a href="/gear" title="Gear">Gear</a></li>
+        <li><a href="/outlet" title="Outlet">Outlet</a></li>
+        <li><a href="/gift-vouchers" title="Gift Vouchers">Gift Vouchers</a></li>
+      </ul>
+
+      <h5 class="mt-24">Legal</h5>
+      <ul>
+        <li>Privacy Policy</li>
+        <li>Gift Voucher Terms</li>
+      </ul>
+    </div>
+
+    <div class="col">
+      <h5>Get in touch</h5>
+      <ul>
+        <li><a href="tel:+4401275349449" class="">+44 (0) 1275 349 449</a></li>
+        <li><a href="mailto:mail@alfaholics.com" class="">mail@alfaholics.com</a></li>
+        <li><a class="" href="tel:011441275349449">01144 1275 349 449</a>&nbsp;<strong>(USA)</strong></li>  
+        <li><a class="" href="tel:0011441275349449">001 144 1275 349 449</a>&nbsp;<strong>(AUS)</strong></li>
+      </ul>
+      <h5 class="mt-24">Location</h5>
+      <p>
+        Unit 12, Hither Green Industrial Estate
+        <br />Clevedon, North Somerset
+        <br />BS21 6XU
+      </p>
+    </div>
+  </div>
+
+  <div class="social_media">
+    <ul>
+      <li>
+        <a href="https://www.instagram.com/alfaholicsltd/" aria-label='Instagram'>
+          <svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+            <path d="M12,4.622c2.403,0,2.688,0.009,3.637,0.052c0.877,0.04,1.354,0.187,1.671,0.31c0.42,0.163,0.72,0.358,1.035,0.673 c0.315,0.315,0.51,0.615,0.673,1.035c0.123,0.317,0.27,0.794,0.31,1.671c0.043,0.949,0.052,1.234,0.052,3.637 s-0.009,2.688-0.052,3.637c-0.04,0.877-0.187,1.354-0.31,1.671c-0.163,0.42-0.358,0.72-0.673,1.035 c-0.315,0.315-0.615,0.51-1.035,0.673c-0.317,0.123-0.794,0.27-1.671,0.31c-0.949,0.043-1.233,0.052-3.637,0.052 s-2.688-0.009-3.637-0.052c-0.877-0.04-1.354-0.187-1.671-0.31c-0.42-0.163-0.72-0.358-1.035-0.673 c-0.315-0.315-0.51-0.615-0.673-1.035c-0.123-0.317-0.27-0.794-0.31-1.671C4.631,14.688,4.622,14.403,4.622,12 s0.009-2.688,0.052-3.637c0.04-0.877,0.187-1.354,0.31-1.671c0.163-0.42,0.358-0.72,0.673-1.035 c0.315-0.315,0.615-0.51,1.035-0.673c0.317-0.123,0.794-0.27,1.671-0.31C9.312,4.631,9.597,4.622,12,4.622 M12,3 C9.556,3,9.249,3.01,8.289,3.054C7.331,3.098,6.677,3.25,6.105,3.472C5.513,3.702,5.011,4.01,4.511,4.511 c-0.5,0.5-0.808,1.002-1.038,1.594C3.25,6.677,3.098,7.331,3.054,8.289C3.01,9.249,3,9.556,3,12c0,2.444,0.01,2.751,0.054,3.711 c0.044,0.958,0.196,1.612,0.418,2.185c0.23,0.592,0.538,1.094,1.038,1.594c0.5,0.5,1.002,0.808,1.594,1.038 c0.572,0.222,1.227,0.375,2.185,0.418C9.249,20.99,9.556,21,12,21s2.751-0.01,3.711-0.054c0.958-0.044,1.612-0.196,2.185-0.418 c0.592-0.23,1.094-0.538,1.594-1.038c0.5-0.5,0.808-1.002,1.038-1.594c0.222-0.572,0.375-1.227,0.418-2.185 C20.99,14.751,21,14.444,21,12s-0.01-2.751-0.054-3.711c-0.044-0.958-0.196-1.612-0.418-2.185c-0.23-0.592-0.538-1.094-1.038-1.594 c-0.5-0.5-1.002-0.808-1.594-1.038c-0.572-0.222-1.227-0.375-2.185-0.418C14.751,3.01,14.444,3,12,3L12,3z M12,7.378 c-2.552,0-4.622,2.069-4.622,4.622S9.448,16.622,12,16.622s4.622-2.069,4.622-4.622S14.552,7.378,12,7.378z M12,15 c-1.657,0-3-1.343-3-3s1.343-3,3-3s3,1.343,3,3S13.657,15,12,15z M16.804,6.116c-0.596,0-1.08,0.484-1.08,1.08 s0.484,1.08,1.08,1.08c0.596,0,1.08-0.484,1.08-1.08S17.401,6.116,16.804,6.116z"></path>
+          </svg>
+        </a>
+      </li>
+
+      <li>
+        <a href="https://www.youtube.com/alfaholicstv" aria-label='Youtube'>
+          <svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+            <path d="M21.8,8.001c0,0-0.195-1.378-0.795-1.985c-0.76-0.797-1.613-0.801-2.004-0.847c-2.799-0.202-6.997-0.202-6.997-0.202 h-0.009c0,0-4.198,0-6.997,0.202C4.608,5.216,3.756,5.22,2.995,6.016C2.395,6.623,2.2,8.001,2.2,8.001S2,9.62,2,11.238v1.517 c0,1.618,0.2,3.237,0.2,3.237s0.195,1.378,0.795,1.985c0.761,0.797,1.76,0.771,2.205,0.855c1.6,0.153,6.8,0.201,6.8,0.201 s4.203-0.006,7.001-0.209c0.391-0.047,1.243-0.051,2.004-0.847c0.6-0.607,0.795-1.985,0.795-1.985s0.2-1.618,0.2-3.237v-1.517 C22,9.62,21.8,8.001,21.8,8.001z M9.935,14.594l-0.001-5.62l5.404,2.82L9.935,14.594z"></path>
+          </svg>
+        </a>
+      </li>
+
+      <li>
+        <a href="https://www.facebook.com/alfaholics" aria-label='Facebook'>
+          <svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+            <path d="M12 2C6.5 2 2 6.5 2 12c0 5 3.7 9.1 8.4 9.9v-7H7.9V12h2.5V9.8c0-2.5 1.5-3.9 3.8-3.9 1.1 0 2.2.2 2.2.2v2.5h-1.3c-1.2 0-1.6.8-1.6 1.6V12h2.8l-.4 2.9h-2.3v7C18.3 21.1 22 17 22 12c0-5.5-4.5-10-10-10z"></path>
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="legal">
+    <p>
+      &copy; Alfaholics Limited {new Date().getFullYear()} - Alfaholics Limited is registered in England and Wales. Registered Number: 03850063
+      <br />
+      Registered office: Unit 12, Hither Green Industrial Estate, Clevedon, North Somerset, BS21 6XU, UK
+    </p>
+  </div>
+
+</footer>
+
+<style type="text/css">
+  footer {
+    overflow: hidden;
+    margin-top:0;
+    border-top:#C9E1F2 solid 2px;
+    display:block;
+    font-size:0.9em;
+  }
+  footer .wrap {
+    max-width:1340px;
+    margin:0 auto;
+    padding:30px 20px;
+    display:flex;
+    width:100%;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  footer .wrap .col {
+    width:25%;
+    flex-basis:25%;
+    flex-grow:0;
+    margin:20px 0;
+  }
+  footer .social_media {
+    margin-bottom:20px;
+  }
+  footer .social_media ul {
+    list-style-type:none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+  footer .social_media li {
+    margin:0 10px;
+    padding:0;
+  }
+  footer .social_media a {
+    display: block;
+    position: relative;
+    width:24px;
+    height:24px;
+  }
+  footer .social_media svg {
+    width:24px;
+    height:24px;
+    fill:var(--theme-color);
+  }
+  footer a {
+    text-decoration: none;
+    color:#222;
+  }
+  footer .mt-24 {
+    margin-top:24px;
+  }
+  footer svg {
+    width:140px;
+    height: 200px;
+  }
+  footer h5 {
+    font-size:1.1em;
+    color:#416691;
+    font-weight:500;
+    font-family:"PP Pangram Sans";
+    text-transform: uppercase;
+    margin:0 0 10px;
+  }
+  footer p {
+    line-height:1.6em;
+    margin-bottom:0;
+  }
+  footer ul {
+    margin:0;
+    padding:0;
+    list-style-type: none;
+  }
+  footer ul li {
+    padding-top: 6px;
+    margin: 6px 0 0;
+    padding: 6px 0 0;
+  }
+  footer .legal {
+    background-color:#122E49;
+    color:#FFF;
+    box-sizing:border-box;
+    display:block;
+    text-align: center;
+    font-family:"PP Pangram Sans";
+    margin:0;
+    padding:20px 15px;
+    line-height:1.4em;
+  }
+  footer .legal p {
+    font-size:0.85em;
+    margin:0;
+  }
+  @media (max-width: 781px) {
+    footer .wrap .col {
+      flex-basis: 50%;
+    }
+    footer .wrap .col:first-child,
+    footer .wrap .col:last-child {
+      flex-basis: 100%;
+    }
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores'
+	import { Header, Footer } from '$lib/templates'
+
+	let { children } = $props();
+	
+	const globalSettings = $page.data.globalSettings
+
+</script>
+
+<svelte:head>
+	<title>Daniel Groves • Bristol</title>
+</svelte:head>
+
+<Header />
+<div id="content">
+	{@render children?.()}
+</div>
+<Footer />
