296d0c9dc584085d6509d90bb74e50e101d79faa
dag995 <72547889+dag995@users.noreply.github.com>
2025-09-09 11:43:42 +0100

    homepage content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <div class="container mx-auto">
+
   <h1>DanielGroves.co.uk</h1>
+
   <div class="row">
     <div class="col-4">
-
       <h2>Overture</h2>
       <p>The turn of the millenium felt like a catalyst for an emerging industry. Where once small cottage business making products and providing services had the opportunity to compete on a level playing-field.</p>
       <p>The internet allowed for anonymous businesses whose size no longer mattered and physical presence could be completely disconnected to its digital shopwindow.</p>
@@ -15,16 +16,48 @@
       <h2>Emerging</h2>
       <p>This new-world order meant that these skilled businesses could reach bigger audiences and begin to showcase work in a way that was beyond word of mouth or regional advertising.</p>
       <p>By working with a website designer or developer they could promote product and service digitally to the masses. Allowing geographic limitations to be shattered overnight.</p>
-      <p>Scaling and diversifying a businesses could be done quickly and easily and economically they could afford to compete with industry titans.</p>
+      <p>Scaling and diversifying a businesses could be done quick and easily. Economically they could afford to compete with industry titans.</p>
     </div>
 
     <div class="col-4">
       <h2>Success</h2>
       <p>This new competitive edge allowed for small businesses not only take business from big industry but also undercut, outperform and deliver quality and value to the end consumer.</p>
-      <p>The economics of small production, micro-services and low cost aquisition made global reach a much more affordable on both sides.</p>
-      <p>This rise was the make or break globalisation changed the course of lives and changed the world forever!</p>
+      <p>The economics of small production, micro-services and low cost aquisition made global reach much more affordable from both sides.</p>
+      <p>This rise was the make or break for some and the globalisation changed the course of lives and businesses the world-over!</p>
       <img src="/images/photography/small_spring_kit_01.jpg" alt="Small Spring Kit" title="Small Spring Kit" />
     </div>
+  </div>
+  
+  <hr />
+
+  <div class="row">
+    <div class="col-4">
+      <h2>First-Hand</h2>
+      <p>Having witnessed this change first hand it became clear that this was the future.</p>
+      <p>With an appetite to learn and a thirst for connecting with people it didn't take long before wanting to breakout of the 9-5 structure working for someone else.</p>
+      <p>Wanting to control the process from start to finish and avoid the fragmentation of how web businesses grew I choose to keep it small and tight.</p>
+      <p>Avoiding the shortcomings of a salesman selling and a designer having to design what had been sold and a developer to then make it work I choose to take on all roles.</p>
+      <p>This confidence in ones self allowed for no overstretch, overpromising and underdelivering but a complete realistic handle on what was promised and delivered.</p>
+      <img src="/images/photography/rear_light_spider_s3_01.jpg" alt="Rear Lights • S3 Spider" title="Rear Lights • S3 Spider" />
+    </div>
+    <div class="col-4">
+      <img src="/diagrams/footrest.jpg" alt="Aluminium Footrest" title="Aluminium Footrest" />
+      <h2>Limitations</h2>
+      <p>The limitations this brought were time which inturn meant resource. The very nature of avoiding the middle management and constant handing off of work meant expectations had to be managed and practicalities realised.</p>
+      <p>This choice to help only a few and to do it well required a long-term outlook and relationships formed with only those I truely got along with and wanted to help.</p>
+      <p>Control of end-to-end implementation does provide free choice over the most practical implementation of solutions, developing out tools that are truely unique to the problem in-hand.</p>
+      <p>This approach is well-fitting to SME businesseses who need results in a realistic timeframe that have impactful and lasting effects on day-to-day running.</p>
+    </div>
+    <div class="col-4">
+      <h2>Second Wave</h2>
+      <p>As the internet matured and large agencies started to develop tools which eventually became open-sourced, robust and available for all to utilise.</p>
+      <p>WordPress became the dominant force for providing websites for all sizes with WooCommerce providing a reliable eCommerce solution from within WordPress.</p>
+      <p>Javascript frameworks (React, Angular, SvelteKit, Next.js etc) emerged to make clever dynamic system available for much more affordable costs.</p>
+      <p>These frameworks much like the internets effect on small businesses allowed for small agencies to provide much more value for money in a shorter window.</p>
+      <p>Where big businesses used to charged tens to hundreds of thousands, small agencies could provide the same level of resource for a fraction of the cost.</p>
+      <img src="/diagrams/studs_into_bell.jpg" alt="Studs into Bell" title="Studs into Bell" />
+    </div>
+
   </div>
 
   <hr />
@@ -33,7 +66,7 @@
   <div class="row">
     <div class="col-4">
       <h3>01. Who</h3>
-      <p>Working with a range to Small and Medium-sized Enterprises to help better their digital presence and business processes.</p>
+      <p>Working with a range of Small and Medium-sized Enterprises (SMEs) to help better their digital presence and business processes.</p>
       <p>With a background in helping small service based businesses and warehouse eCommerce businesses who fulfill B2B and B2C orders directly.</p>
       <p>Supporting a wide varierty of industries who typically have the same structure and goals.</p>
     </div>
